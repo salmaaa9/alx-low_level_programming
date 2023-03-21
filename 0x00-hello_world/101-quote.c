5a35e232c8cebf385ee10a9768d5ea7ecfe1fa08
salmaaa9 <smsmss279201@yahoo.com>
2023-03-20 18:06:54 -0700

    hh

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,16 @@
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ *description : a c programming that prints using puts
+ *
+ * Return: Always 1 (Success)
+*/
+
+int main(void)
+{
+	char q[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(l, q, 59);
+	return (1);
+}

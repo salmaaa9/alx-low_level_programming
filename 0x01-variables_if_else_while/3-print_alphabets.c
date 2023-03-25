aa96534b2de3f95e7ca46e1aa5b52499acdfa2b2
salmaaa9 <smsmss279201@yahoo.com>
2023-03-25 16:58:55 -0700

    hh

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -entry point
+ *Description:check no is positive negative or zero
+ *Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		putchar (i);
+	}
+	for (i = 65; i < 91; i++)
+	{
+		putchar (i);
+	}
+	putchar(10);
+	return (0);
+}

c260355c6a8ac5c623be811e6af1426314632d35
salmaaa9 <smsmss279201@yahoo.com>
2023-03-25 17:19:11 -0700

    no

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -entry point
+ *Description:check no is positive negative or zero
+ *Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 122; i > 96; i--)
+	{
+		putchar(i);
+	}
+	putchar(10);
+	return (0);
+}

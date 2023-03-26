96ad257cf385818088300319a0828a15e97261c0
salmaaa9 <smsmss279201@yahoo.com>
2023-03-25 17:10:20 -0700

    gg

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main -entry point
+ *Description:check no is positive negative or zero
+ *Return: 0 (success)
+*/
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	printf("\n");
+	return (0);
+}

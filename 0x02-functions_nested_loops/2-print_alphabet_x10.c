494b2a156c89d1642e6d379c1e17b51054292883
salmaaa9 <smsmss279201@yahoo.com>
2023-03-26 17:30:27 -0700

    char

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the charachters lowercased
+ * ten times using _putchar
+ *Return: 1 (success)
+ *        -1 on error
+ */
+void print_alphabet(void)
+{
+	int i, j;
+
+	for (j = 0; j < 11; j++)
+	{
+		for (i = 97; i < 123; i++)
+			_putchar(i);
+			_putchar('\n');
+	}
+}

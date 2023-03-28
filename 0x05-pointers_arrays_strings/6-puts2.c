e5db94dc5190c9971673e9fcaf1997c6fe96bea2
salmaaa9 <smsmss279201@yahoo.com>
2023-03-27 21:03:38 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ *void puts2 - prints every other charachter
+ *@str:an array
+ * Return :nothing
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+		i=i+2;
+	}
+	_putchar('\n');
+}

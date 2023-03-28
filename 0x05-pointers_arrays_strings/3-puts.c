b826c365ba6887baeb98a8e2c1d9c148298150c1
salmaaa9 <smsmss279201@yahoo.com>
2023-03-27 18:41:59 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _puts - prints string
+ *@str:an array
+ * Return :nothing
+ */
+
+void _puts(char *str)
+{
+	while (str[i] != '\0')
+		-putchar(str[i]);
+	-putchar('\n');
+}

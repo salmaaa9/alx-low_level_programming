d323bd2cd599a3af5af4a6d6e95da720752d7270
salmaaa9 <smsmss279201@yahoo.com>
2023-03-27 19:02:25 -0700

    pointers

--- 0x05-pointers_arrays_strings/_putchar.c
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -1,6 +1,12 @@
 #include <unistd>
 #include "main.h"
 
+/**
+ * _putchar - prints char
+ *@c:a character to be printed
+ * Return :1 success
+ *         -1 otherwise
+ */
 int _putchar(char c)
 {
         return (write(1, &c, 1));

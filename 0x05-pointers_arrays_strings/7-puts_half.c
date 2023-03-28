b5476c8c2323352d2fda425898bdb8f18faa82f2
salmaaa9 <smsmss279201@yahoo.com>
2023-03-28 15:05:12 -0700

    pointers

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,8 +11,8 @@ void puts_half(char *str)
 
 	while (str[i] != '\0')
 		i++;
-
-	for (j = (i - 1) / 2; str[j] != '\0'; j++)
+	n = (i - 1) / 2;
+	for (j = n; str[j] != '\0'; j++)
 	{
 		_putchar(str[j]);
 	}

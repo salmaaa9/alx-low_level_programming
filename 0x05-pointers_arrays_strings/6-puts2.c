f06485733c6aba9d00432944bbd10c912f888f88
salmaaa9 <smsmss279201@yahoo.com>
2023-03-28 13:10:37 -0700

    pointers

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -12,7 +12,7 @@ void puts2(char *str)
 	for (i = 0; str[i] != '\0'; i++)
 	{
 		_putchar(str[i]);
-		if (str[i + 2] != '\0')
+		if (str[i + 2] != '\0' && str[i + 1] != '\0')
 		i = i + 1;
 		else
 			break;

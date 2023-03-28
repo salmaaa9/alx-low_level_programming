a47ec500be36f06d340ecd5067395035941dfcdc
salmaaa9 <smsmss279201@yahoo.com>
2023-03-28 16:55:07 -0700

    pointers

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strcpy - copies a string to string
+ *@dest:a pointer to string
+ *@src:a pointer to string
+ * Return : a pointer to string
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	dest = src;
+	return dest;
+
+}

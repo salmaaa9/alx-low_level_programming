c3316c86dc7b9d72607a27dbfb5e56fdf59cab83
salmaaa9 <smsmss279201@yahoo.com>
2023-03-31 20:36:17 -0700

    hh

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes lower case to upper
+ *@s:a string
+ * Return: string
+ */
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if(s[i] > 96 && s[i] < 123)
+		{
+			s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}

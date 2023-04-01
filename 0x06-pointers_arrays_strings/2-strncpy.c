cd4c6985db86897115b494267b799965a71fd5cf
salmaaa9 <smsmss279201@yahoo.com>
2023-03-31 17:04:53 -0700

    hh

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _strncpy - copies a string to another
+ *@dest:a string
+ *@src:a string
+ *@n: an integer
+ * Return: copied string
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int j;
+
+	for (j = 0; j < n; j++)
+	{
+		dest[j] = src[j];
+	}
+	for (j; dest[j] != '\0'; j++)
+	{
+		dest[j] = dest[j];
+	}
+	dest[j] = '\0';
+	return (dest);
+}

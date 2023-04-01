a9e723287e38265c0688c8b314fd40642ba37e0a
salmaaa9 <smsmss279201@yahoo.com>
2023-03-31 18:17:48 -0700

    hh

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,30 +1,29 @@
 #include "main.h"
 
 /**
- * _strncpy - copies a string to another
- *@dest:a string
- *@src:a string
- *@n: an integer
- * Return: copied string
+ * _strcmp - compares a string to another
+ *@s1:a string
+ *@s2:a string
+ * Return: an int comparison
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i,c1,c2;
+	int i, c1, c2;
 
 	for (i = 0; s1[i] != '\0' && s2[i] != '\0'; i++)
 	{
-		c1 =s1[i]-'A';
-		c2 =s2[i]-'A';
+		c1 = s1[i] - 'A';
+		c2 = s2[i] - 'A';
 		if (c1 == c2)
 			continue;
-		else if ( c1 < c2)
+		else if (c1 < c2)
 		{
 			break;
 		}
-		else if ( c1 > c2)
+		else if (c1 > c2)
 		{
 			break;
 		}
 	}
-	return (c1-c2);
+	return (c1 - c2);
 }

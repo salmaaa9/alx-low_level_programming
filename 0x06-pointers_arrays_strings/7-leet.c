40e10953e9596a94197aea4cba581571a8ea5433
salmaaa9 <smsmss279201@yahoo.com>
2023-03-31 21:45:26 -0700

    hh

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * leet - create code
+ *@s:a string
+ * Return: coded string
+ */
+char *leet(char *s)
+{
+	int i;
+	char map[] = {'A', 'E', 'O', 'T', 'L'};
+	int x[] = {4, 3, 0, 7, 1};
+
+	while (*s)
+	{
+		for (i = 0; i < sizeof(map) / sizeof(char); i++)
+		{
+			if (s[i] == map[i] || s[i] == map[i] + 32)
+				s[i] = 48 + x[i];
+		}
+	s++;
+	}
+	return (s);
+}

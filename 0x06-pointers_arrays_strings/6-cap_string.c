04e86d32f24fde64c4934d2dc1f7c27512f81dd5
salmaaa9 <smsmss279201@yahoo.com>
2023-03-31 21:15:39 -0700

    hh

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * cap_string - capitilaizies string
+ *@s:a string
+ * Return:  string
+ */
+
+char *cap_string(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i - 1] == ' ' || s[i - 1] == '\n' || s[i - 1] == '\t'
+			|| s[i - 1] == '.' ||  s[i - 1] == ',' ||
+			s[i - 1] == '?' || s[i - 1] == ';' || s[i - 1] == '!'
+			|| s[i - 1] == '('
+			|| s[i - 1] == ')' || s[i - 1] == '{' || s[i - 1] == '}')
+		{
+			if (s[i] > 96 && s[i] < 123)
+				s[i] = s[i] - 32;
+		}
+	}
+	return (s);
+}

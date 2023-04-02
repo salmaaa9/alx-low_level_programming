db9298e5e71ce0813aa5c5cd4a4114d49990ba03
salmaaa9 <smsmss279201@yahoo.com>
2023-04-02 16:15:33 -0700

    hh

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,12 +9,12 @@ int print_last_digit(int n)
 {
 	if (n < 10)
 	{
-		printf("%d", (n % 10) * -1);
+		_putchar('0' + (n % 10) * -1);
 		return ((n % 10) * -1);
 	}
 	else
 	{
-		printf("%d", n % 10);
+		_putchar('0' + n % 10);
 		return (n % 10);
 	}
 }

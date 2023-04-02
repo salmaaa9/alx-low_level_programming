00d415e78e5634b00bd37e3d982c83d825afe1ed
salmaaa9 <smsmss279201@yahoo.com>
2023-04-02 14:29:38 -0700

    hh

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates two strings till n
+ *@dest:a string
+ *@src:a string
+ *@n: an int
+ * Return: concatened string
+ */
+int print_last_digit(int n)
+{
+	return (n % 10);
+}

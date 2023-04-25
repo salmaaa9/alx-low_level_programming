0ddced72a822f121417372b9f8c8041f8ea68b47
salmaaa9 <smsmss279201@yahoo.com>
2023-04-25 11:14:31 -0700

    hh

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+* print_remaining_days - takes a date and prints how many days are
+* left in the year, taking leap years into account
+* @month: month in number format
+* @day: day of month
+* @year: year
+* Return: void
+*/
+char *_memcpy(char *dest, char *src, unsigned int n);

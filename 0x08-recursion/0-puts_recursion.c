5310a7724bad6360b361cd9d081b4f6814074b67
salmaaa9 <smsmss279201@yahoo.com>
2023-04-26 15:05:55 -0700

    edited

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - prints recurrsivily
+ *@s:a string
+ * Return: nothing
+*/
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+		return;
+	printf("%c", *s);
+	s++;
+	_puts_recursion(s)
+}

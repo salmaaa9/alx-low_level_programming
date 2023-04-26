baf63afe9216afd852bf70908ca2582947fe61ca
salmaaa9 <smsmss279201@yahoo.com>
2023-04-26 13:45:15 -0700

    edited

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _strspn - counts no of char
+ *@s:a string
+ *@accept:A string
+ * Return:no of char
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	int i = 0, j;
+	unsigned int n = 0;
+
+	while (accept[i] != '\0')
+	{
+		for (j = 0; s[j] != '\0'; j++)
+		{
+			if (s[j] == accept[i])
+			{
+				n++;
+				break;
+			}
+		}
+		i++;
+	}
+	n++;
+	return (n);
+}
